415bc5ad6db63014373571c3d18bb02b51c825bc
Diego Lincoln <diego@mutualtecnologia.com.br>
2025-06-07 00:22:40 -0300

    chore: bump version to 0.1.1 and update translation file structure
    - Updated version in package.json to 0.1.1.
    - Changed translationFiles from Map to Set for improved handling of unique translation keys.
    - Modified parameter types in translation function to support both string and number types.

--- apps/cli/src/templates/translation.ts
+++ apps/cli/src/templates/translation.ts
@@ -26,7 +26,7 @@ export const translationFile = ({ languages }: TranslationProps): Map<string, st
 
 `;
 
-  const translationFiles = new Map<string, string>();
+  const translationFiles = new Set<string>();
   const translationsFns = new Map<string, TranslationFn[]>();
 
   const outputFiles = new Map<string, string>();
@@ -44,7 +44,7 @@ export const translationFile = ({ languages }: TranslationProps): Map<string, st
 
       for (const input of translationInputs) {
         fnParams += `${input[1].replace(/\{|\}/g, '').trim()}, `;
-        fnParamsTypes += `${input[1].replace(/\{|\}/g, '').trim()}: string, `;
+        fnParamsTypes += `${input[1].replace(/\{|\}/g, '').trim()}: string | number, `;
       }
 
       if (fnParams.endsWith(', ')) {
@@ -64,16 +64,11 @@ export const translationFile = ({ languages }: TranslationProps): Map<string, st
 
       translationsFns.set(key, [...currentTranslationFns, translationFn]);
 
-      let fileContent = translationFiles.get(key) ?? '';
-
-      fileContent += ` * @${language} ${translation}
-`;
-     
-      translationFiles.set(key, fileContent);
+      translationFiles.add(key);
     }
   }
 
-  for (const [key, fileContent] of translationFiles.entries()) {
+  for (const [key] of translationFiles.entries()) {
     const translationFns = translationsFns.get(key) ?? [];
 
     const translationTypes = translationFns.reduce((acc, i) => {
@@ -100,7 +95,7 @@ export const translationFile = ({ languages }: TranslationProps): Map<string, st
       throw new Error(`Translation content for key ${key} is undefined`);
     }
 
-    translationContent += `export const ${key} = (inputs: ${translationTypesAsArray.length > 0 ? `{ ${translationTypesAsArray.map(type => `${type}: string`).join(', ')} }` : '{} = {}'}, locale?: string) => {
+    translationContent += `export const ${key} = (inputs: ${translationTypesAsArray.length > 0 ? `{ ${translationTypesAsArray.map(type => `${type}: string | number`).join(', ')} }` : '{} = {}'}, locale?: string) => {
   const _locale = locale ?? getCurrentLanguage();
 
   ${translationFns.map(fn => `if (_locale === '${fn.language}') return ${fn.fnName}(${fn.fnParams ? 'inputs' : ''});`).join('\n  ')}
